c3c31b7e6b4dd542df2bfbbb0e1b9ee29df60565
Vladimir Eroshenko <admin@it-vi.ru>
2020-01-02 00:05:16 +0300

    DB 1 of 3

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,8 @@ import csv
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
 from phones.models import Phone
 
 
@@ -25,6 +27,6 @@ class Command(BaseCommand):
                 device.price = int(line[3])
                 device.release_date = datetime.fromisoformat(line[4])
                 device.lte_exists = line[5]
-                device.slug = str(device.name).lower().replace(' ', '-')
+                device.slug = slugify(device.name, allow_unicode=True)
                 device.save()
 
